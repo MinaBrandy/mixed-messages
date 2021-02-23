54769ee9bcd226dfcffb28da59bebcc0f65f0075
MinaBrandy <m_m_9_6@hotmail.com>
2021-02-23 10:40:40 -0800

    Variables and output added

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let living = ['House', 'Apartment', 'Farmhouse', 'Basement']
 let numOfKids = ['Two', 'Four', 'Six', 'Eight']
 
 const fortune = () => {
-    let trueColour = colour(Math.floor(Math.random() * colour.length))
-    let trueLiving = living(Math.floor(Math.random() * living.length))
-    let trueNumOfKids = numOfKids(Math.floor(Math.random() * numOfKids.length))
+    let trueColour = colour[Math.floor(Math.random() * colour.length)]
+    let trueLiving = living[Math.floor(Math.random() * living.length)]
+    let trueNumOfKids = numOfKids[Math.floor(Math.random() * numOfKids.length)]
 
-    console.log(`Your fortune it is fair, your favourite colour will be ${trueColour}, you will live in a ${trueLiving}, and you will have ${trueNumOfKids} kids.`)
+    return `Your fortune it is fair, your favourite colour will be ${trueColour}, you will live in a ${trueLiving}, and you will have ${trueNumOfKids} kids.`
 }
+
+console.log(fortune())
