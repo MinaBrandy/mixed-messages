9dbb2fc91f31200a46d4480133ed4f0b039a2ba6
MinaBrandy <m_m_9_6@hotmail.com>
2021-02-23 10:28:59 -0800

    First mock up of fortune teller entered

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('It works!')
+let colour = ['Pink', 'Blue', 'Red', 'Purple']
+let living = ['House', 'Apartment', 'Farmhouse', 'Basement']
+let numOfKids = ['Two', 'Four', 'Six', 'Eight']
+
+const fortune = () => {
+    let trueColour = colour(Math.floor(Math.random() * colour.length))
+    let trueLiving = living(Math.floor(Math.random() * living.length))
+    let trueNumOfKids = numOfKids(Math.floor(Math.random() * numOfKids.length))
+
+    console.log(`Your fortune it is fair, your favourite colour will be ${trueColour}, you will live in a ${trueLiving}, and you will have ${trueNumOfKids} kids.`)
+}
